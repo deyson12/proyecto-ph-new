fd09864b112615f3ee4d9f78d9b432ce15a4cf73
Deyson Estrada <deyson12@gmail.com>
2024-10-22 16:16:21 -0500

    Ajustes para funcionar con nueva version de agular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
-import { CUSTOM_ELEMENTS_SCHEMA, isDevMode, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { isDevMode, NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router'; // Importa el RouterModule
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { provideStore } from '@ngrx/store';
+import { ActionReducer, StoreModule } from '@ngrx/store';
 import { structure } from './app.reducer';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { TokenInterceptor } from './interceptor/interceptor.service';
 import { FullCalendarComponent, FullCalendarModule } from '@fullcalendar/angular';
 import { Booking2Component } from './components/booking2/booking2/booking2.component';
 import { BookingComponent } from './components/booking/booking/booking.component';
+import { localStorageSync } from 'ngrx-store-localstorage';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+
+export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
+  return localStorageSync({ keys: ['user'], rehydrate: true })(reducer);
+}
 
 @NgModule({
   declarations: [
@@ -21,12 +26,14 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    FullCalendarModule,
     FormsModule,
-    RouterModule.forRoot(routes)
+    FullCalendarModule,
+    RouterModule.forRoot(routes),
+    StoreModule.forRoot(structure, {
+      metaReducers: [localStorageSyncReducer], // Sincroniza el estado del store con localStorage
+    }),
   ],
   providers: [
-    provideStore(structure), 
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() , connectInZone: true}),
     provideHttpClient(withInterceptors([TokenInterceptor]))
   ],
